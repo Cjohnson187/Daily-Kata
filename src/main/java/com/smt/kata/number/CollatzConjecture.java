78005dc6bc29191b21fd4fa7d89cd6b12e28524f
cjohnson187 <chrisfjohnson3@gmail.com>
2021-04-01 10:24:42 -0600

    finished fast kata 1 min over

--- src/main/java/com/smt/kata/number/CollatzConjecture.java
+++ src/main/java/com/smt/kata/number/CollatzConjecture.java
@@ -54,6 +54,14 @@ public class CollatzConjecture {
 	 * @return
 	 */
 	public int calculate(int n) {
-		return n;
+		if (n<0) n = Math.abs(n);
+		int counter = 0;
+	
+		while(n > 1) {
+			if(n%2 == 0) n = n/2;
+			else n=(n*3) + 1;
+			counter++;
+		}
+		return counter;
 	}
 }
