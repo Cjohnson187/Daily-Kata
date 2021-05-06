46c587c7ca6d8a851e6476f8a333f2d19ca5bc1b
cjohnson187 <chrisfjohnson3@gmail.com>
2021-05-06 10:31:51 -0600

    fin

--- src/main/java/com/smt/kata/code/AnagramIndices.java
+++ src/main/java/com/smt/kata/code/AnagramIndices.java
@@ -1,7 +1,11 @@
 package com.smt.kata.code;
 
+import java.util.List;
+import java.util.ArrayList;
 // JDK 11.x
 import java.util.Collection;
+
+import com.siliconmtn.data.text.StringUtil;
 
 /****************************************************************************
  * <b>Title:</b> AnagramIndicese.java
@@ -30,6 +34,44 @@ public class AnagramIndices {
 	 * @return Collection of indices locations.  Empty collection if none found
 	 */
 	public Collection<Integer> find(String w, String s) {
-		return null;
+		List<Integer> in = new ArrayList<>();
+		String s2 = s;
+		if (StringUtil.isEmpty(s) || StringUtil.isEmpty(w))
+			return in;
+		int index = 0;
+		while (s.contains(w)) {
+			
+			System.out.println("s1 = " + s +" index of w  + index"+(s.indexOf(w) + index ));
+			in.add((s.indexOf(w)+index));
+			
+			//index = (s.indexOf(w)+ index + w.length()); 
+			index = (s.indexOf(w) + w.length()); 
+			
+			s = s.substring(s.indexOf(w) +w.length() );
+			
+			
+			System.out.println(index+" s = " + s +" "+(s.indexOf(w) + index));
+			
+		}
+		StringBuilder a = new StringBuilder(s2);
+		String w2 = a.reverse().toString();
+		while (s.contains(w2)) {
+			
+			System.out.println("s1 = " + s +" index of w  + index"+(s.indexOf(w2) + index ));
+			in.add((s.indexOf(w2)+index));
+			
+			//index = (s.indexOf(w)+ index + w.length()); 
+			index = (s.indexOf(w2) + w2.length()); 
+			
+			s = s.substring(s.indexOf(w2) +w2.length() );
+			
+			
+			System.out.println(index+" s = " + s +" "+(s.indexOf(w2) + index));
+			
+		}
+		
+		
+		
+		return in;
 	}
 }
