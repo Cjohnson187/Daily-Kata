727f5b4609cfa8f23cd374855291f96d3d7fe335
cjohnson187 <chrisfjohnson3@gmail.com>
2021-08-11 10:39:03 -0600

    saving

--- src/main/java/com/smt/kata/database/ExcelGenerator.java
+++ src/main/java/com/smt/kata/database/ExcelGenerator.java
@@ -3,7 +3,11 @@ package com.smt.kata.database;
 // JDK 11.x
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -45,8 +49,29 @@ public class ExcelGenerator {
 	 * @throws IOException
 	 * @throws SQLException
 	 */
-	public byte[] getContactReport(String actionGroupId) 
-	throws IOException, SQLException {
+	public byte[] getContactReport(String actionGroupId) throws IOException, SQLException {
+		Map<String, String> headerMap = new HashMap<>();
+		
+		StringBuilder sql = new StringBuilder();
+		sql.append("select *, count(sa.action_id) as num from sb_action sa ");
+		sql.append("inner join contact c on sa.action_id = c.action_id ");
+		sql.append("inner join contact_submittal cs on c.action_id = cs.action_id ");
+		sql.append("group by sa.action_id where action_group_id ");
+		sql.append("='").append(actionGroupId).append("'");
+		
+		try (PreparedStatement ps = conn.prepareStatement(sql.toString())) {
+			ResultSet rs = ps.executeQuery();
+			
+			for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+				
+			}
+			
+			
+		} catch (Exception e) {
+			System.err.println("Error getting results");
+			e.printStackTrace();
+		}
+		//ExcelReport report = new ExcelReport(headers);
 		return new byte[0];
 	}
 	
@@ -57,10 +82,39 @@ public class ExcelGenerator {
 	 * @return Map of action_group_id and count(*) of items
 	 * @throws SQLException 
 	 */
-	public Map<String, Integer> getContactCountByOrganization(String orgId) 
-	throws SQLException {
+	public Map<String, Integer> getContactCountByOrganization(String orgId) throws SQLException {
 		if (StringUtils.isEmpty(orgId)) throw new SQLException("Organization ID is required");
+		
 		Map<String, Integer> data = new LinkedHashMap<>();
+		
+		StringBuilder sql = new StringBuilder();
+		sql.append("select sa.action_group_id , count(sa.action_id )  from sb_action sa  ");
+		sql.append("inner join contact c ");
+		sql.append("on sa.action_id = c.action_id ");
+		sql.append("inner join contact_submittal cs ");
+		sql.append("on c.action_id = cs.action_id  ");
+		sql.append("where sa.organization_id = ? ");
+		sql.append("group by sa.action_group_id ");
+		
+		
+		try (PreparedStatement ps = conn.prepareStatement(sql.toString())) {
+			ps.setString(1, orgId);
+			
+			
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				System.out.println(orgId + "  result group id   " + rs.getString(1) + " count = " + Integer.parseInt(rs.getString(2)));
+				data.put(rs.getString(1), Integer.parseInt(rs.getString(2)));
+				
+			}
+			
+			
+			
+		} catch (Exception e) {
+			System.err.println("Error getting results");
+			e.printStackTrace();
+		}
+		
 		return data;
 	}
 }
