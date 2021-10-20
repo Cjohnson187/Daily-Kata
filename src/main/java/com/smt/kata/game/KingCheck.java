8db7ffad1470720db9848fcbeac9f8b7972f3011
cjohnson187 <chrisfjohnson3@gmail.com>
2021-10-20 10:18:21 -0600

    saving

--- src/main/java/com/smt/kata/game/KingCheck.java
+++ src/main/java/com/smt/kata/game/KingCheck.java
@@ -1,5 +1,10 @@
 package com.smt.kata.game;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.smt.kata.distance.bean.CoordinateVO;
+
 /****************************************************************************
  * <b>Title</b>: KingCheck.java
  * <b>Project</b>: SMT-Kata
@@ -40,22 +45,187 @@ package com.smt.kata.game;
  * @updates:
  ****************************************************************************/
 public class KingCheck {
-	char[][] board;
-	
-	/**
-	 * Assigns the chess board to the class
-	 * @param board Chess board
-	 */
-	public KingCheck(char[][] board) {
-		this.board = board;
-	}
+    char[][] board;
+    
+    List<Character> pieces = new ArrayList<>();    
+    
+    CoordinateVO kingPos = null;
+    CoordinateVO queenPos = null;
+    CoordinateVO pawnPos = null;
+    CoordinateVO bishopPos = null;
+    CoordinateVO rookPos = null;
+    CoordinateVO knightPos = null;
+    
+    /**
+     * Assigns the chess board to the class
+     * @param board Chess board
+     */
+    public KingCheck(char[][] board) {
+        this.board = board;
+    }
 
-	/**
-	 * 
-	 * @param board
-	 * @return
-	 */
-	public boolean isKingCheck() {
-		return board == null;
-	}
+    /**
+     * 
+     * @param board
+     * @return
+     */
+    public boolean isKingCheck() {
+        initializePieces();
+        
+        kingPos = locatePieces('K', pieces, board);
+        queenPos = locatePieces('Q', pieces, board);
+        pawnPos = locatePieces('P', pieces, board);
+        bishopPos = locatePieces('B', pieces, board);
+        rookPos = locatePieces('R', pieces, board);
+        knightPos = locatePieces('N', pieces, board);
+        
+        if(kingPos.getRow() == -1 || kingPos.getColumn() == -1)
+        	return false;
+        
+        return isKingCheckByBishop(kingPos, bishopPos) || isKingCheckByKnight(kingPos, knightPos) || isKingCheckByPawn(kingPos, pawnPos) || isKingCheckByRook(kingPos, rookPos) || isKingCheckByQueen(kingPos, queenPos);
+        
+//        return board == null;
+    }
+    
+    public void initializePieces() {
+        pieces.add('K');
+        pieces.add('Q');
+        pieces.add('P');
+        pieces.add('B');
+        pieces.add('R');
+        pieces.add('N');
+    }
+    
+    public CoordinateVO locatePieces(Character peice, List<Character> pieces, char[][] board) {
+        CoordinateVO vo = new CoordinateVO();
+        for(int i=0; i<board.length; i++) {
+            for(int j=0; j<board[0].length; j++) {
+                if(String.valueOf(board[i][j]).equals(String.valueOf(peice))) {
+                    vo.setRow(i);
+                    vo.setColumn(j);
+                    
+                    return vo;
+                }
+                    
+            }
+        }
+        
+        if(vo.getRow() < 0)
+        	vo.setRow(-1);
+        if(vo.getColumn() < 0)
+        	vo.setRow(-1);
+        return vo;
+    }
+    
+    public boolean isKingCheckByRook(CoordinateVO kingPos, CoordinateVO rookPos) {
+    	if(rookPos.getRow() == -1 || rookPos.getColumn() == -1)
+    		return false;
+    	
+        int row = rookPos.getRow();
+        int column = rookPos.getColumn();
+
+        for (int i = 0; i < 8; i++) {
+            
+            for (int j = 0; j < 8; j++) {
+                
+                if (kingPos.equals(new CoordinateVO(row+i, column))
+                || (kingPos.equals(new CoordinateVO(row, column-j)))
+                || (kingPos.equals(new CoordinateVO(row, column+j)))
+                || (kingPos.equals(new CoordinateVO(row-i, column)))) {
+
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+
+    public boolean isKingCheckByBishop(CoordinateVO kingPos, CoordinateVO bishopPos) {
+    	if(bishopPos.getRow() == -1 || bishopPos.getColumn() == -1)
+    		return false;
+    	
+        int row = bishopPos.getRow();
+        int column = bishopPos.getColumn();
+
+        for (int i = 0; i < 8; i++) {
+            
+            for (int j = 0; j < 8; j++) {
+                
+                if (kingPos.equals(new CoordinateVO(row+i, column+j))
+                || (kingPos.equals(new CoordinateVO(row+i, column-j)))
+                || (kingPos.equals(new CoordinateVO(row-i, column+j)))
+                || (kingPos.equals(new CoordinateVO(row-i, column-j)))) {
+
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    
+    public boolean isKingCheckByQueen(CoordinateVO kingPos, CoordinateVO queenPos) {
+    	if(queenPos.getRow() == -1 || queenPos.getColumn() == -1)
+    		return false;
+    	
+        return isKingCheckByBishop(kingPos, queenPos) || isKingCheckByRook(kingPos, queenPos);
+    }
+
+    public boolean isKingCheckByKnight(CoordinateVO kingPos, CoordinateVO knightPos) {
+    	if(knightPos.getRow() == -1 || knightPos.getColumn() == -1)
+    		return false;
+    	
+        List<CoordinateVO> knightPositions = new ArrayList<>();
+        // up right
+        knightPositions.add(new CoordinateVO(knightPos.getRow()+2, knightPos.getColumn()+1)  );
+        // right up
+        knightPositions.add(new CoordinateVO(knightPos.getRow()+1, knightPos.getColumn()+2)  );
+        
+        // up left
+        knightPositions.add(new CoordinateVO(knightPos.getRow()+2, knightPos.getColumn()-1)  );
+        // left up
+        knightPositions.add(new CoordinateVO(knightPos.getRow()+1, knightPos.getColumn()-2)  );
+        
+        
+        // down right
+        knightPositions.add(new CoordinateVO(knightPos.getRow()-2, knightPos.getColumn()+1)  );
+        // right down
+        knightPositions.add(new CoordinateVO(knightPos.getRow()-1, knightPos.getColumn()+2)  );
+        
+        // down left
+        knightPositions.add(new CoordinateVO(knightPos.getRow()-2, knightPos.getColumn()-1)  );
+        // left down
+        knightPositions.add(new CoordinateVO(knightPos.getRow()-1, knightPos.getColumn()-2)  );
+        
+        for(CoordinateVO vo : knightPositions) {
+            if(kingPos.getRow() == vo.getRow() &&  kingPos.getColumn() == vo.getColumn()) {
+                return true;
+            }
+        }
+    
+        return false;
+    }
+    
+    public boolean isKingCheckByPawn(CoordinateVO kingPos, CoordinateVO pawnPos) {
+    	if(pawnPos.getRow() == -1 || pawnPos.getColumn() == -1)
+    		return false;
+    	
+        int row = pawnPos.getRow();
+        int col = pawnPos.getColumn();
+        
+        for(int i=0; i<8; i++) {
+            for(int j=0; j<8; j++) {
+                if (kingPos.equals(new CoordinateVO(row+1, col+1))
+                        || (kingPos.equals(new CoordinateVO(row-1, col-1)))
+                        || (kingPos.equals(new CoordinateVO(row+1, col-1)))
+                        || (kingPos.equals(new CoordinateVO(row-1, col+1)))) {
+
+                            return true;
+                        }
+            }
+        }
+        
+        return false;
+    }
 }
--- src/main/java/com/smt/kata/number/ValidSudoku.java
+++ src/main/java/com/smt/kata/number/ValidSudoku.java
@@ -1,57 +1,43 @@
 package com.smt.kata.number;
 
 /****************************************************************************
- * <b>Title</b>: ValidSudoku.java
- * <b>Project</b>: SMT-Kata
- * <b>Description: </b> Valid Sudoku
+ * <b>Title</b>: ValidSudoku.java <b>Project</b>: SMT-Kata <b>Description: </b>
+ * Valid Sudoku
  * 
- * Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be 
+ * Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be
  * validated according to the following rules:
  * 
- * - Each row must contain the digits 1-9 without repetition.
- * - Each column must contain the digits 1-9 without repetition.
- * - Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+ * - Each row must contain the digits 1-9 without repetition. - Each column must
+ * contain the digits 1-9 without repetition. - Each of the nine 3 x 3 sub-boxes
+ * of the grid must contain the digits 1-9 without repetition.
  * 
- * Note:
- * A Sudoku board (partially filled) could be valid but is not necessarily solvable.
- * Only the filled cells need to be validated according to the mentioned rules.
+ * Note: A Sudoku board (partially filled) could be valid but is not necessarily
+ * solvable. Only the filled cells need to be validated according to the
+ * mentioned rules.
  * 
  * Example 1
  * https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Sudoku-by-L2G-20050714.svg/250px-Sudoku-by-L2G-20050714.svg.png
- * Input: board = 
- * [["5","3",".",".","7",".",".",".","."]
- * ,["6",".",".","1","9","5",".",".","."]
- * ,[".","9","8",".",".",".",".","6","."]
- * ,["8",".",".",".","6",".",".",".","3"]
- * ,["4",".",".","8",".","3",".",".","1"]
- * ,["7",".",".",".","2",".",".",".","6"]
- * ,[".","6",".",".",".",".","2","8","."]
+ * Input: board = [["5","3",".",".","7",".",".",".","."]
+ * ,["6",".",".","1","9","5",".",".","."] ,[".","9","8",".",".",".",".","6","."]
+ * ,["8",".",".",".","6",".",".",".","3"] ,["4",".",".","8",".","3",".",".","1"]
+ * ,["7",".",".",".","2",".",".",".","6"] ,[".","6",".",".",".",".","2","8","."]
  * ,[".",".",".","4","1","9",".",".","5"]
- * ,[".",".",".",".","8",".",".","7","9"]]
- * Output: true
+ * ,[".",".",".",".","8",".",".","7","9"]] Output: true
  * 
- * Example 2:
- * Input: board = 
- * [["8","3",".",".","7",".",".",".","."]
- * ,["6",".",".","1","9","5",".",".","."]
- * ,[".","9","8",".",".",".",".","6","."]
- * ,["8",".",".",".","6",".",".",".","3"]
- * ,["4",".",".","8",".","3",".",".","1"]
- * ,["7",".",".",".","2",".",".",".","6"]
- * ,[".","6",".",".",".",".","2","8","."]
+ * Example 2: Input: board = [["8","3",".",".","7",".",".",".","."]
+ * ,["6",".",".","1","9","5",".",".","."] ,[".","9","8",".",".",".",".","6","."]
+ * ,["8",".",".",".","6",".",".",".","3"] ,["4",".",".","8",".","3",".",".","1"]
+ * ,["7",".",".",".","2",".",".",".","6"] ,[".","6",".",".",".",".","2","8","."]
  * ,[".",".",".","4","1","9",".",".","5"]
- * ,[".",".",".",".","8",".",".","7","9"]]
- * Output: false
- * Explanation: Same as Example 1, except with the 5 in the top left corner being 
- * modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+ * ,[".",".",".",".","8",".",".","7","9"]] Output: false Explanation: Same as
+ * Example 1, except with the 5 in the top left corner being modified to 8.
+ * Since there are two 8's in the top left 3x3 sub-box, it is invalid.
  * 
- * Constraints:
- * board.length == 9
- * board[i].length == 9
- * board[i][j] is a digit 1-9 or '.'.
+ * Constraints: board.length == 9 board[i].length == 9 board[i][j] is a digit
+ * 1-9 or '.'.
  * 
- * <b>Copyright:</b> Copyright (c) 2021
- * <b>Company:</b> Silicon Mountain Technologies
+ * <b>Copyright:</b> Copyright (c) 2021 <b>Company:</b> Silicon Mountain
+ * Technologies
  * 
  * @author James Camire
  * @version 3.0
@@ -59,13 +45,71 @@ package com.smt.kata.number;
  * @updates:
  ****************************************************************************/
 public class ValidSudoku {
-	
+
 	/**
 	 * Determines if the values in the Sudoku board are valid
+	 * 
 	 * @param board Sudoku board to evaluate
-	 * @return True if the values are valid.  False otherwise
+	 * @return True if the values are valid. False otherwise
 	 */
 	public boolean isValid(String[][] board) {
-		return board == null;
+		boolean result = true;
+		
+		result = this.checkRow(board);
+		if(!result) return false;
+		
+		result  = this.checkCol(board);
+		if(!result) return false;
+		
+		for (int i = 0; i < board.length; i+=3) {
+			for (int j = 0; j < board[i].length; j+=3) {
+				result = this.checkBox(board, i, j);
+				if(!result) return false;
+			}
+		}
+		
+		return result;
+	}
+
+	public boolean checkRow(String[][] board) {
+		for (int i = 0; i < board.length; i++) {
+			StringBuilder numB = new StringBuilder(128);
+			for (int j = 0; j < board[i].length; j++) {
+				if (!board[i][j].equals(".") && -1 == numB.indexOf(board[i][j]))
+					numB.append(board[i][j]);
+				else if (!board[i][j].equals(".")&& numB.indexOf(board[i][j]) >= 0)
+					return false;
+			}
+		}
+		return true;
+	}
+
+	public boolean checkCol(String[][] board) {
+		for (int i = 0; i < board[0].length; i++) {
+			StringBuilder numB = new StringBuilder(128);
+			for (int j = 0; j < board.length; j++) {
+				if (!board[j][i].equals(".") && -1 == numB.indexOf(board[j][i]))
+					numB.append(board[j][i]);
+				else if (!board[j][i].equals(".") && numB.indexOf(board[j][i]) >= 0)
+					return false;
+
+			}
+		}
+		return true;
+	}
+	
+	
+	public boolean checkBox(String[][] board, int row, int col) {
+		StringBuilder numB = new StringBuilder(128);
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (!board[i+row][j+col].equals(".") && -1 == numB.indexOf(board[i+row][j+col]))
+					numB.append(board[i+row][j+col]);
+				else if (!board[i+row][j+col].equals(".") && numB.indexOf(board[i+row][j+col]) >= 0) {
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 }
